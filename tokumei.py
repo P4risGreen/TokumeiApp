4ad7e6c7d7c0395886c533da0a1d39987a3c95c5
木山祐希 <[hokagen.hp@gmail.com]>
2022-02-13 12:26:48 +0900

    add all file

--- tokumei.py
+++ tokumei.py
@@ -4,8 +4,10 @@ import urllib.request
 s = ""
 print("何話までDLした？:")
 nanwa = int(input()) + 1
+print("何話までDLする？:")
+made = int(input())
 
-for i in range(248 - nanwa):
+for i in range(made - nanwa):
     s = str(i+nanwa).zfill(3)
     print("#"+s+"をダウンロード中")
     urllib.request.urlretrieve("https://omocoro.heteml.net/radio/tokumei/"+s+".mp3","/Users/yk001/environment/tokumeiradio/"+s+".mp3")
